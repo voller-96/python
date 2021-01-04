2c41e71b1a213eec9f15907a6e2ebf4858b6bad9
Voller Silas <voller.silas@gmail.com>
2021-01-04 15:49:43 -0300

    Primeiro capítulo

---
+++ cap1.py
@@ -0,0 +1,20 @@
+print("Hello Python!")
+print (5+2)
+
+print("#############################")
+minutos = 42
+segundos = minutos*60 + 42
+print("Há",segundos,"segundos em ",minutos,"minutos mais 42 segundos.")
+
+print("#############################")
+km = 10
+milhas = round(km/1.61,2)
+print("O número de milhas em km é :",milhas,"milhas.")
+
+print("##################################")
+passo = milhas/segundos
+print(passo,"por segundo.")
+
+print("##################################")
+passo2 = milhas*360/segundos
+print(passo2,"milhas por hora.")
